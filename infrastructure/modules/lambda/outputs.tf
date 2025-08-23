63846ff468365bc2fe454415d7caf9e7a38f57ff
codejitsu <me@codejitsu.net>
2025-08-23 21:50:53 +0200

    Remove unused outputs.

--- infrastructure/modules/lambda/outputs.tf
+++ infrastructure/modules/lambda/outputs.tf
@@ -7,8 +7,3 @@ output "function_name" {
   description = "The name of the GraphQL API Lambda function"
   value       = aws_lambda_function.graphql_api_lambda.function_name
 }
-
-output "lambda_execute_role_arn" {
-  description = "The ARN of the IAM role that allows Lambda execution"
-  value       = aws_iam_role.lambda_execute_role.arn
-}

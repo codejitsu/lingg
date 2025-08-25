bbb7cb4ec4644063f461642e1f3127c8f858c95b
codejitsu <me@codejitsu.net>
2025-08-25 22:50:47 +0200

    Refactor API schema: add user, chapter and language types.

--- backend/graphql-api-lambda/src/main.rs
+++ backend/graphql-api-lambda/src/main.rs
@@ -1,6 +1,7 @@
 use lambda_appsync::appsync_lambda_main;
 use lambda_appsync::ID;
 use lambda_appsync::{appsync_operation, AppsyncError, AppsyncEvent};
+use uuid::Uuid;
 
 // Generate types and runtime setup from schema
 appsync_lambda_main!(
@@ -12,18 +13,26 @@ appsync_lambda_main!(
 
 #[appsync_operation(mutation(startStory), with_appsync_event)]
 async fn start_story(
-    target_language_code: String,
-    explain_language_code: String,
-    story_type: String,
+    args: StartStoryArguments,
     _event: &AppsyncEvent<Operation>
 ) -> Result<Story, AppsyncError> {
-    let story = Story {
-        story_id: ID::new(),
-        target_language_code: target_language_code,
-        explain_language_code: explain_language_code,
-        story_type: story_type,
+    let story_id = Uuid::now_v7();
+    let chapter_id = Uuid::now_v7();
+
+    let chapter = Chapter {
+        chapter_id: ID::try_from(chapter_id.to_string()).unwrap(),
+        story_id: ID::try_from(story_id.to_string()).unwrap(),
         content: "Once upon a time...".to_string(),
         created_at: "2023-10-01T00:00:00Z".to_string().into(),
+    };
+
+    let story = Story {
+        story_id: ID::try_from(story_id.to_string()).unwrap(),
+        target_language: args.target_language,
+        explain_language: args.explain_language,
+        story_type: args.story_type,
+        started_at: "2023-10-01T00:00:00Z".to_string().into(),
+        chapters: vec![chapter],
     };
 
     Ok(story)
